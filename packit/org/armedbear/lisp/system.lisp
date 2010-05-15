ba97ff4a68d05c5bd631f4f7a0c191289bcb78cc
Alan Ruttenberg <alanruttenberg@gmail.com>
2010-05-15 06:13:58 +0000

    remove dependency from directory-in-jar

--- packit/org/armedbear/lisp/system.lisp
+++ packit/org/armedbear/lisp/system.lisp
@@ -9,30 +9,6 @@
 (setq *default-pathname-defaults* (pathname "/"))
 (setq asdf::*defined-systems* (make-hash-table :test 'equal))
 (setq asdf::*asdf-verbose* nil)
-;(setq *debugger-hook* #'system::%debugger-hook-function)
-
-
-(defun directory-in-jar (pathname)
-  (let* ((device (pathname-device pathname))
-	 (jarfile (namestring (car device)))
-	 (rest-pathname (namestring (make-pathname :directory `(:absolute ,@(cdr (pathname-directory pathname)))
-						   :name (pathname-name pathname)
-						   :type (pathname-type pathname)))))
-    (if (or (position #\* (namestring rest-pathname))
-	    (wild-pathname-p rest-pathname))
-	(let ((jar (jnew  "java.util.zip.ZipFile" jarfile)))
-	  (let ((els (jcall "entries" jar)))
-	    (loop while (jcall "hasMoreElements" els)
-	       for name = (jcall "getName" (jcall "nextElement" els))
-	       when (pathname-match-p (concatenate 'string "/" name) rest-pathname)
-	       collect (make-pathname :device (pathname-device pathname)
-				      :name (pathname-name name)
-				      :type (pathname-type name)
-				      :directory `(:absolute ,@(cdr (pathname-directory name)))))))
-	(let ((truename (probe-file pathname)))
-	  (if truename
-              (list truename)
-              nil)))))
 
 (defun abcl-module-candidates ()
   (remove-duplicates
@@ -48,7 +24,7 @@
 (defvar *loading-jarfile*)
 
 (loop for jar in  (abcl-module-candidates)
-   for (candidate) = (directory-in-jar (format nil "jar:file:~a!/abcl-module-*.lisp" (namestring (merge-pathnames jar (format nil "~a/" (jstatic "getProperty" "java.lang.System" "user.dir"))))))
+   for (candidate) = (directory (format nil "jar:file:~a!/abcl-module-*.lisp" (namestring (merge-pathnames jar (format nil "~a/" (jstatic "getProperty" "java.lang.System" "user.dir"))))))
    when candidate
    do (let ((*loading-jarfile* (make-pathname :device (pathname-device candidate))))
 	(load candidate)
