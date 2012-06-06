8cf5cdf2ff51a4845b0288e9bb01703b7cab4410
jonathanbona@gmail.com <jonathanbona@gmail.com@2fb5c00c-6cf9-8e71-a64e-e8ceb770abcf>
2012-06-06 20:14:35 +0000

    invoke-restargs wasn't reading nil as false. this should be fixed in abcl itself eventually

--- branches/bona/patches/abcl-jss-fix.lisp
+++ branches/bona/patches/abcl-jss-fix.lisp
@@ -42,12 +42,12 @@
 (in-package :jss)
 
 (defun invoke-restargs (method object args &optional (raw? nil))
-;;  (pprint (type-of args))
   (let* ((object-as-class-name 
           (if (symbolp object) (maybe-resolve-class-against-imports object)))
          (object-as-class 
           (if object-as-class-name (find-java-class object-as-class-name))))
     (setf args (map 'list (lambda (x) (if (eq t x) +true+ x)) args)) ; make all the ts +true+
+    (setf args (map 'list (lambda (x) (if (eq nil x) +false+ x)) args)) ; make all the nils +false+
     (if (eq method 'new)
         (apply #'jnew (or object-as-class-name object) args)
         (if raw?
