7f052d61f8b78884edb3bfa1c0470671a9c3ab23
Alan Ruttenberg <alanruttenberg@gmail.com>
2012-08-17 21:37:54 +0000

    ongoing

---
+++ bioportal/annotator.lisp
@@ -0,0 +1,17 @@
+;; this is the key associated with the account "lsw". You might want to get your own key,
+;; by creating an account on bioportal.
+
+(defvar *bioportal-key* "6551953c-31bb-4189-91b5-f0733a251c61")
+
+(defun ncbo-annotate (text apikey &key (format "XML"))
+  (get-url "http://rest.bioontology.org/obs/annotator" 
+	   :post (append 
+		  (list 
+		   (list "apikey" apikey)
+		   (list "textToAnnotate" text))
+		  (when format (list (list "format" format))))))
+
+(defun test-ncbo-annotator ()
+  (let ((sample "This protocol will evaluate patients with systemic lupus erythematosus (SLE) and their relatives"))
+    (xmls::parse (ncbo-annotate sample *bioportal-key* :format "XML"))))
+
