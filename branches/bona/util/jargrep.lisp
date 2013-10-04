029feb2cfd14c54258758f75f8d7dfdd756c7636
alanruttenberg@gmail.com <alanruttenberg@gmail.com@2fb5c00c-6cf9-8e71-a64e-e8ceb770abcf>
2013-10-04 20:54:55 +0000

    improvements

--- branches/bona/util/jargrep.lisp
+++ branches/bona/util/jargrep.lisp
@@ -1,29 +1,53 @@
 ;; Author: Alan Ruttenberg
 ;; Date: September 24, 2013
 
-(defun jar-map (jar-or-jars fn)
+(defun jar-map (jar-or-jars fn &key (read-type :buffer))
   "given a jar file or a list of jar files, call fn on the string that is the decompressed entry.
 TODO: Add filtering by path name, so we can look only in, say, the XML files"
+  (catch 'abort-jar-map
     (loop for jar in (if (consp jar-or-jars) jar-or-jars (list jar-or-jars))
        with buffer-size = 0
        with buffer = nil
        for jarfile = (new 'jarfile (new 'file (namestring (truename jar))))
        for entries = (#"entries" jarfile)
        do
-	 (loop while (#"hasMoreElements" entries)
-	    for next-in =  (#"nextElement" entries)
-	    for in-stream = (#"getInputStream" jarfile next-in)
-	    for size = (#"getSize" next-in)
-	    do
-	      (when (> size buffer-size) 
-		(setq buffer (jnew-array "byte" size)))
-	      (when (> size 0)
-		(#"read" in-stream buffer)
-		(setq @ buffer)
-		(unwind-protect
-		     (let ((name (#"getName" next-in)))
-		       (funcall fn (new 'java.lang.string buffer size) name))
-		  (#"close" in-stream))))))
+       (loop while (#"hasMoreElements" entries)
+	  for next-in =  (#"nextElement" entries)
+	  for in-stream = (#"getInputStream" jarfile next-in)
+	  for size = (#"getSize" next-in)
+	  do
+					;(print-db size)
+	  (when (> size 0)
+	    (when (and (eq read-type :buffer) (> size buffer-size) )
+	      (setq buffer (jnew-array "byte" size)))
+	    (cond ((eq read-type :buffer)
+		   (progn 
+		     (loop with total = 0
+			for read = (#"read" in-stream buffer total (- size total))
+			until (>= total size)
+			do (incf total read))
+		     (setq @ buffer)
+		     (unwind-protect
+			  (let ((name (#"getName" next-in)))
+			    (funcall fn (new 'java.lang.string buffer 0 size "UTF-8") name))
+		       (#"close" in-stream))))
+		  ((eq read-type :lisp-stream)
+		   (let ((lisp-stream (new 'stream 'system::stream (new 'BufferedReader (new 'InputStreamReader in-stream)))))
+		     (unwind-protect
+			  (let ((name (#"getName" next-in)))
+			    (funcall fn in-stream name))
+		       (#"close" in-stream)
+		       (close lisp-stream)
+		       )))
+		  ((eq read-type :inputstream)
+		     (unwind-protect
+			  (let ((name (#"getName" next-in)))
+			    (funcall fn in-stream name))
+		       (#"close" in-stream)
+		       ))
+		  ((t (error "don't know read-type other than :buffer and :stream")))))
+	  )
+       )))
 
 
 ;; Create a thread for each jar file. Each thread executes
@@ -38,12 +62,13 @@ TODO: Add filtering by path name, so we can look only in, say, the XML files"
 	     (loop
 		for i from 0 below nthreads
 		for f in jar-filenames
-		collect (threads:make-thread
-			 (lambda()
-			   (funcall thread-run-function f))
-			 :name (format nil "~a~a" thread-name-prefix i)))
+		collect (let ((f f))
+			  (threads:make-thread
+			   (lambda()
+			     (funcall thread-run-function f))
+			   :name (format nil "~a~a" thread-name-prefix i))))
 	     do (threads:thread-join thread)))
-  (print (hash-table-count *hits*)))
+  )
 
 ;; One global variable to hold our results hash
 (defvar *hits*)
@@ -103,3 +128,49 @@ TODO: Add filtering by path name, so we can look only in, say, the XML files"
   (let ((format-string (#"replaceFirst" template "#" (format nil "~~~a,'0d" digits))))
     (loop for i from from to to collect (format nil format-string i))))
 
+(defvar *xml-errors*)
+
+(defun jar-map-xml-notes (jar-filenames xml-fn) ; &key (threads (length jar-filenames)))
+  (thread-per-jar
+   (lambda(jarfile)
+     (let ((factory (#"newInstance" 'DocumentBuilderFactory)))
+       (#"setNamespaceAware" factory nil)
+       (jar-map 
+	jarfile
+	(lambda(stream name)
+	  (when (#"matches" name ".*\.xml$")
+	    (multiple-value-bind (res errorp)
+		(ignore-errors
+		  (let* ((builder  (#"newDocumentBuilder" factory))
+			 (doc (#"parse" builder (new 'InputSource (new 'BufferedReader (new 'InputStreamReader stream))))))
+		     (funcall xml-fn doc name))))))
+	:read-type :inputstream)))
+   jar-filenames))
+
+
+(defvar *doc2forms*)
+(defun compute-note-2-forms (jar-filenames)
+  (setq *doc2forms* (make-hash-table :test 'equal))
+  (jar-map-xml-notes 
+   jar-filenames 
+   (lambda(doc entry)
+     (let ((forms
+	    (loop for node being the cloned-nodeset-nodes of (xpath:query doc "//Form")
+	       for id = (#"getAttribute" node "id")
+	       for name = (#"getAttribute" node "name")
+	       collect (list name id))))
+       (setf (gethash entry *doc2forms*) forms)))))
+
+#|
+
+;; sample query
+
+
+
+(defun pdb-chem-info (pdbxml)
+  (loop for node being the cloned-nodeset-nodes of (xpath:query pdbxml "//PDBx:chem_comp")
+     for id = (#"getAttribute" node "id")
+     for (formula name) = (xpath:elements node "PDBx:formula" "PDBx:name")
+     collect (list id name formula (third (assoc id *chebi-pdb-chems* :test 'equal)))))
+
+|#
