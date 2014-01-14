7e638144faac94563a30715236f160605d748370
alanruttenberg <alanruttenberg@2fb5c00c-6cf9-8e71-a64e-e8ceb770abcf>
2014-01-14 16:15:56 +0000

    look in directories at the same level as lsw: for other .asd files

--- branches/bona/scripts/system-registry.lisp
+++ branches/bona/scripts/system-registry.lisp
@@ -15,6 +15,27 @@
      if (equal (pathname-type top) "asd")
      do (pushnew (namestring directory)  asdf::*central-registry* :test 'equal)))
 
+(defun register-all-parallel-sysdefs (directory) 
+  "assuming directory in repository folder, find all other systems in ../*/"
+  (let ((loaded (loop for k being the hash-keys of ASDF/FIND-SYSTEM:*DEFINED-SYSTEMS* collect k))
+	(seen nil)
+	(them (ignore-errors 
+		(directory (merge-pathnames (make-pathname :directory '(:relative :up :wild) :type "asd" :name :wild) (truename directory))))))
+    (loop for path in them 
+	 for system-name =  (pathname-name path)
+	 do
+	 (if (member system-name loaded :test 'equalp) ;; don't add a second time
+	     (progn 
+	       (push path seen)
+	       (warn "asd ~a is already loaded, so not loading ~a"  (pathname-name path) path))
+	     (let ((already (find system-name seen :test 'equalp :key 'pathname-name)))
+	       (if already
+		 (warn "~a is already loaded, so not loading ~a" already path)
+		 (progn 
+		   (format t "Registering sysdef ~a~%" path)
+		   (pushnew (directory-namestring path) asdf::*central-registry* :test 'equalp)
+		   (push path seen))))))))
+
 (setf (logical-pathname-translations "lsw")
       `(("**;*.*" ,(make-pathname :directory (append (butlast (pathname-directory *load-pathname*))
 						     '(:wild-inferiors))
@@ -22,7 +43,7 @@
 				  :type :wild))))
 
 (register-all-asdf-sysdefs (logical-pathname "lsw:"))
-
+(register-all-parallel-sysdefs (logical-pathname "lsw:"))
 
 (defvar *lsw-configuration* nil)
 
