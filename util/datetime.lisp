f3f078d7f5925420eabfdf6704fcd1abc6e24daa
Alan Ruttenberg <alanruttenberg@gmail.com>
2014-01-22 09:04:05 +0000

    add today-iso-date -> 2014-01-22

--- util/datetime.lisp
+++ util/datetime.lisp
@@ -5,3 +5,9 @@
 	     (new 'java.util.Date)
 	     (new 'stringbuffer "")
 	     (new 'fieldposition (get-java-field 'java.text.DateFormat "DATE_FIELD")))))
+
+(defun today-iso-date ()
+  (multiple-value-bind (second minute hour day month year dow dst zone)
+      (decode-universal-time (get-universal-time))
+    (declare (ignore second hour minute dow dst zone))
+    (format nil "~4,'0D-~2,'0D-~2,'0D" year month day)))
