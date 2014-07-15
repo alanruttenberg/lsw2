aef315b402c80941ea0ec8d0cc0f019513971c3c
Alan Ruttenberg <alanruttenberg@gmail.com>
2014-07-15 06:14:59 +0000

    Summary: Add some functions for table introspection
    ;; let's assume this works for all connections until we know otherwise
    ;; documentation http://docs.oracle.com/javase/7/docs/api/java/sql/DatabaseMetaData.html
    
    ;; return a list of pairs of table name and comment if available
    (defun table-names (connection)
    
    ;; return a list of pairs of columns and comment if available
    (defun table-columns (connection table)
    
    ;; return a hash of table-name to list of column-names
    (defun get-all-tables-and-columns (connection)
    
    ;; return list of forms (source-table source-column target-table
    ;; target-column), where the source column is a foreign key to the
    ;; target-table and target column. Note this includes both foreign
    ;; keys in and out of the table passed as argument to the function
    (defun table-references (connection table)
    
    ;; return a list of tables with foreign keys into table
    (defun which-tables-reference (connection table)
    
    ;; return a list of pairs of tables and commment where the table name matches regex
    (defun table-names-matching (connection regex)
    
    ;; return a list of triples of tables, commment, and comment on comment where the column name matches regex
    (defun column-names-matching (connection regex)

--- util/jdbc.lisp
+++ util/jdbc.lisp
@@ -115,16 +115,79 @@ INNER JOIN sys.columns c ON t.OBJECT_ID = c.OBJECT_ID
 where t.name like '~a' and c.name like '~a'
 ORDER BY schema_name, table_name;" table-match  column-match) connection))
 
+
+;; let's assume this works for all connections until we know otherwise
+;; documentation http://docs.oracle.com/javase/7/docs/api/java/sql/DatabaseMetaData.html
+
+;; return a list of pairs of table name and comment if available
 (defun table-names (connection)
-  (cond ((equal "com.hxtt.sql.access.r" (jclass-name (jobject-class connection)))
-	 (let ((arr (jnew-array "java.lang.String" 1)))
-	   (jarray-set arr "TABLE" 0)
-	   (loop with tables = 
-		(#"getTables" (#"getMetaData" c) +null+ +null+ "%" arr)
-	      for next? = (#"next" tables)
-	      until (not next?) do (print (#"getString" tables 3))
-		)))
-	(t (error "table-names not supported for other than access database at the moment"))))
+  (let ((arr (jnew-array "java.lang.String" 1)))
+    (jarray-set arr "TABLE" 0)
+    (loop with tables = 
+	 (#"getTables" (#"getMetaData" connection) +null+ +null+ "%" arr)
+       for next? = (#"next" tables)
+       until (not next?)
+       if (member (#"getString" tables 5) '(nil "") :test 'equal)
+       collect (list (#"getString" tables 3))
+       else collect (list (#"getString" tables 3) (#"getString" tables 5))
+    )))
+
+;; return a list of pairs of columns and comment if available
+(defun table-columns (connection table)
+  (loop with tables = 
+       (#"getColumns" (#"getMetaData" connection) +null+ +null+ table "%")
+     for next? = (#"next" tables)
+     until (not next?)
+     if (member (#"getString" tables 12) '(nil "") :test 'equal)
+     collect (list (#"getString" tables 4) )
+     else collect (list (#"getString" tables 4) (#"getString" tables 12) )
+       ))
+
+;; return a hash of table-name to list of column-names
+(defun get-all-tables-and-columns (connection)
+  (loop with tables = (make-hash-table :test 'equal)
+     with meta = (#"getColumns" (#"getMetaData" connection) +null+ +null+ "%" "%")
+     for next? = (#"next" meta)
+     until (not next?)
+     do (push  (list (#"getString" meta 4) (#"getString" meta 12)) (gethash  (#"getString" meta 3) tables))
+     finally (return tables)))
+
+;; return list of forms (source-table source-column target-table
+;; target-column), where the source column is a foreign key to the
+;; target-table and target column. Note this includes both foreign
+;; keys in and out of the table passed as argument to the function
+(defun table-references (connection table)
+  (loop for (target) in (table-names connection) append
+       (loop with meta = (#"getCrossReference" (#"getMetaData" connection) +null+ +null+ table +null+ +null+ target)
+	  for next? = (#"next" meta)
+	  until (not next?)
+	  collect (list (#"getString" meta 7)  (#"getString" meta 8) (#"getString" meta 3)  (#"getString" meta 4) )
+	    )
+     append
+       (loop with meta = (#"getCrossReference" (#"getMetaData" connection) +null+ +null+ target +null+ +null+ table)
+	  for next? = (#"next" meta)
+	  until (not next?)
+	  collect (list (#"getString" meta 7)  (#"getString" meta 8) (#"getString" meta 3)  (#"getString" meta 4) )
+	    )))
+
+;; return a list of tables with foreign keys into table
+(defun which-tables-reference (connection table)
+  (let ((refs (table-references connection table)))
+    (remove-duplicates (remove table (mapcar 'car refs) :test 'equal) :test 'equal)))
+
+;; return a list of pairs of tables and commment where the table name matches regex
+(defun table-names-matching (connection regex)
+  (loop for (table comment) in (table-names connection) when (all-matches table regex 0) collect (list table comment)))
+
+;; return a list of triples of tables, commment, and comment on comment where the column name matches regex
+(defun column-names-matching (connection regex)
+  (let ((them nil))
+    (maphash 
+     (lambda(table columns)
+       (let ((matched (loop for (column comment) in columns when (all-matches column regex 0) collect (list table column comment))))
+	 (when matched (setq them (append matched them)))))
+     (get-all-tables-and-columns connection))
+    them))
 
 (defun table-counts (connection tables &optional (threshold 1) &rest fields)
   (loop for table in tables 
