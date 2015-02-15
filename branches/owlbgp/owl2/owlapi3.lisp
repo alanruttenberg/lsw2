7a2d2ad015a6a74baad137ef14eb11afbf8812ac
alanruttenberg <alanruttenberg@2fb5c00c-6cf9-8e71-a64e-e8ceb770abcf>
2015-02-15 18:55:35 +0000

    factor our dl-queries from owlapi3.lisp

--- branches/owlbgp/owl2/owlapi3.lisp
+++ branches/owlbgp/owl2/owlapi3.lisp
@@ -402,43 +402,6 @@
 	 for uri = (and iri (make-uri string))
 	 unless (or (null iri) (and (eq uri !owl:Nothing) (not include-nothing))) collect (make-uri string)))))
 
-
-(defun children (class kb)
-  (class-query class kb (lambda(ce reasoner) (#"getSubClasses" reasoner ce t))))
-
-(defun descendants (class kb)
-  (class-query class kb (lambda(ce reasoner) (#"getSubClasses" reasoner ce nil))))
-
-(defun parents (class kb)
-  (class-query class kb (lambda(ce reasoner) (#"getSuperClasses" reasoner ce t))))
-
-(defun ancestors (class kb)
-  (class-query class kb (lambda(ce reasoner) (#"getSuperClasses" reasoner ce nil))))
-
-(defun instances (class kb)
-  (class-query class kb (lambda(ce reasoner) (#"getInstances" reasoner ce nil))))
-
-(defun direct-instances (class kb)
-  (class-query class kb (lambda(ce reasoner) (#"getInstances" reasoner ce t))))
-
-(defun equivalents (class kb)
-  (class-query class kb (lambda(ce reasoner) (#"getEquivalentClasses" reasoner ce)) nil t))
-
-
-(defun same-individuals (individual kb)
-  (loop for e in (jss::set-to-list
-		  (#"getSameIndividuals" (v3kb-reasoner kb)
-		     (#"getOWLNamedIndividual" (v3kb-datafactory kb) (to-iri individual))))
-       collecting (make-uri (#"toString" (#"getIRI" e)))))
-
-(defun entailed? (axiom-expression kb)
-  (error "todo")
-  (#"isEntailed" (list-to-java-set (list (to-axiom-expression class-expression kb))) kb)) ;not yet implemented
-
-(defun satisfiable? (class-expression kb)
-  (instantiate-reasoner kb)
-  (#"isSatisfiable" (v3kb-reasoner kb) (to-class-expression class-expression kb)))
-
 (defun is-subclass-of? (sub super kb)
   "This is faster than using parents or ancestors as you don't have to classify the ontology in order to test it"
   (not (satisfiable? `(object-intersection-of (object-complement-of ,super) ,sub) kb)))
