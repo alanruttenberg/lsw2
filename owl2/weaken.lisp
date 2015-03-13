2bb3a7568ff82603bdba5c8b5b517fc00239d093
Alan Ruttenberg <alanruttenberg@gmail.com>
2015-03-13 04:53:50 +0000

    add weakening by removing subclassof cardinality restriction

--- owl2/weaken.lisp
+++ owl2/weaken.lisp
@@ -85,3 +85,35 @@
 			   (#"applyChanges"  (v3kb-manager (or target ont)) (v3kb-changes (or target ont))))
 	(setf (v3kb-changes (or target ont)) nil)))
     (or target ont)))
+
+(defun weaken-remove-cardinality (ont &optional new-ontology-name)
+  "modify ont or create a new ontology in which the equivalentclasses axioms are turned into subclassof axioms"
+  (let ((target (and new-ontology-name
+		     (let* ((manager (#"createOWLOntologyManager" 'OWLManager))
+			    (ontology (#"createOntology" manager)))
+		       (make-v3kb :name new-ontology-name
+				  :manager manager
+				  :ont ontology
+				  :datafactory (#"getOWLDataFactory" manager)
+				  :weakened-from ont)))))
+    (unwind-protect
+	 (each-axiom
+	  ont
+	  (lambda(axiom)
+	      (axiom-typecase axiom
+		(subclassof
+		 (if (and (jinstance-of-p (#"getSubClass" axiom) (find-java-class 'OWLClass))
+			  (or (jinstance-of-p (#"getSuperClass" axiom) (find-java-class 'OWLObjectMinCardinality))
+			      (jinstance-of-p (#"getSuperClass" axiom) (find-java-class 'OWLObjectMaxCardinality))
+			      (jinstance-of-p (#"getSuperClass" axiom) (find-java-class 'OWLObjectExactCardinality))))
+		     (unless target
+		       (remove-axiom axiom ont))))
+		(otherwise 
+		 (when target (add-axiom axiom target))))))
+      (unwind-protect (and (v3kb-changes (or target ont))
+			   (#"applyChanges"  (v3kb-manager (or target ont)) (v3kb-changes (or target ont))))
+	(setf (v3kb-changes (or target ont)) nil)))
+    (or target ont)))
+
+
+	    
