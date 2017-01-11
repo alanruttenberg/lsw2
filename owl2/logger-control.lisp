c3559876872afedbe60911d6ec4cc25dbd3ca060
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-01-10 22:34:09 -0500

    Found another noisy logger to muffle

--- owl2/logger-control.lisp
+++ owl2/logger-control.lisp
@@ -1,7 +1,8 @@
 (defun mute-owlapi-loggers ()
   (loop for loggerkey in (list (get-java-field 'slf4j.logger "ROOT_LOGGER_NAME" t)
 			       'org.semanticweb.owlapi.rdf.rdfxml.parser.OWLRDFConsumer
-			       'org.semanticweb.owlapi.io.AbstractOWLParser)
+			       'org.semanticweb.owlapi.io.AbstractOWLParser
+			       'org.semanticweb.owlapi.rdf.rdfxml.parser.AbstractState)
 	for logger = (#"getLogger" 'slf4j.LoggerFactory (if (symbolp loggerkey) (find-java-class loggerkey) loggerkey))
 	do
 	   (set-java-field logger "currentLogLevel" (get-java-field logger "LOG_LEVEL_WARN" t) t)))
