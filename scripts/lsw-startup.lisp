5e7080d79093a0bc6de7ed60c5d2b0fa6e089f19
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-02-06 16:14:19 -0500

    simplify finding asdf systems, quicklisp init

--- scripts/lsw-startup.lisp
+++ scripts/lsw-startup.lisp
@@ -5,21 +5,9 @@
 				  :type :wild
 				  :defaults *load-pathname*))))
 
-(defvar *lsw-ql-directory* 
-  (merge-pathnames (make-pathname :directory '(:relative "quicklisp")) (make-pathname :directory (butlast (pathname-directory  *load-pathname*)))))
+(require :quicklisp-abcl)
 
-(if (probe-file (merge-pathnames "setup.lisp" *lsw-ql-directory*))
-    (load (merge-pathnames "setup.lisp" *lsw-ql-directory*))
-    (progn 
-      (format *debug-io* "; Installing quicklisp to ~s~%" *lsw-ql-directory*)
-      (load "https://beta.quicklisp.org/quicklisp.lisp")
-      (funcall (intern "INSTALL" 'quicklisp-quickstart) :path *lsw-ql-directory*)))
-
-(let ((*load-verbose* nil)
-      (*compile-verbose* nil)
-      (*suppress-compiler-warnings* (not *load-verbose*)))
-  (ql:quickload 'owl2)
-  (ql:quickload 'inspect))
+(ql:quickload 'owl2)
+(ql:quickload 'inspect)
   
-;  (asdf::oos 'asdf::load-op 'owl2 :verbose nil)
-;  (asdf::oos 'asdf::load-op 'inspect :verbose nil))
+
