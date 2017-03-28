545c45b16967c1aa0c610b79e215f48d95ce6890
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-03-28 15:56:56 -0400

    more tests

--- owl2/test-lsw2-owl2.lisp
+++ owl2/test-lsw2-owl2.lisp
@@ -11,7 +11,39 @@
 
     (loop for expression in expressions 
 	  do
-	     (eval `(prove:is-type (to-owlapi-axiom ',expression ,test-ontology) 'java-object)))))
+	     (eval `(prove:is-type (to-owlapi-axiom ',expression ,test-ontology) 'java-object "construct axiom from sexp")))))
+
+(prove:finalize)
+
+(prove:plan 3 "Basic reasoner")
+
+(prove.test::ok
+ (with-ontology f () 
+   ((asq (subclassof !a !b))) 
+   (check-ontology f :classify t))
+ "ontology can be classified")
+
+(prove.test::ok
+ (with-ontology f () 
+   ((asq (declaration (class !a))
+	 (declaration (class !b))
+	 (disjoint-classes !a !b)
+	 (declaration (named-individual !h))
+	 (class-assertion !a !h)
+	 (class-assertion !b !h)))
+   (not (check-ontology f)))
+ "inconsistent ontology recognized")
+
+(prove.test::ok
+ (with-ontology f () 
+   ((asq (declaration (class !a))
+	 (declaration (class !b))
+	 (disjoint-classes !a !b)
+	 (declaration (class !c))
+	 (equivalent-classes !c (and !a !b))
+	 ))
+   (unsatisfiable-classes f))
+ "unsatisfiable class recognized")
 
 (prove:finalize)
 
@@ -26,7 +58,8 @@
  (evurl '((:entailment (sub-class-of !ex:a !owl:Nothing) 
     :support ((sub-class-of !ex:a !ex:b)
 	      (equivalent-classes !ex:b (object-complement-of !ex:a)))
-	   :signature (!ex:b !ex:a)))))
+	   :signature (!ex:b !ex:a))))
+ "explain unsatisfiable class")
 
 (prove:is 
   (with-ontology ontology ()
@@ -36,7 +69,8 @@
   (evurl
    '((:entailment (sub-class-of !owl:Thing !owl:Nothing)
       :support ((sub-class-of !owl:Thing !ex:a) (sub-class-of !ex:a !owl:Nothing))
-      :signature (!owl:Nothing !ex:a !owl:Thing)))))
+      :signature (!owl:Nothing !ex:a !owl:Thing))))
+  "Explain inconsistency")
 
 (prove:finalize)
 
@@ -49,7 +83,8 @@
 	  (equivalent-object-properties !c !d)))
    (instantiate-reasoner f :hermit :config (quiet-reasoner-config))
    (property-equivalents !c))
- (list  !c !d))
+ (list  !c !d)
+ "property-equivalents (object-property)")
 
 (prove::is
  (with-ontology f () 
@@ -58,7 +93,8 @@
 	  (equivalent-data-properties !c !d)))
    (instantiate-reasoner f :hermit :config (quiet-reasoner-config))
    (property-equivalents !c))
- (list  !c !d))
+ (list  !c !d)
+ "property-equivalents (data-property)")
 
 (prove::is
  (with-ontology f () 
@@ -67,7 +103,8 @@
 	 (sub-object-property-of !a !b)))
    (instantiate-reasoner f :hermit :config (quiet-reasoner-config))
    (property-parents !a))
- (list !b))
+ (list !b)
+ "property-parents")
 
 (prove::is
  (with-ontology f () 
@@ -76,7 +113,8 @@
 	 (sub-object-property-of !a !b)))
    (instantiate-reasoner f :hermit :config (quiet-reasoner-config))
    (property-ancestors !a))
- (list !b))
+ (list !b)
+ "property-ancestors")
 
 (prove::is
  (with-ontology f () 
@@ -85,7 +123,8 @@
 	 (sub-object-property-of !a !b)))
    (instantiate-reasoner f :hermit :config (quiet-reasoner-config))
    (property-children !b))
- (list !a))
+ (list !a)
+ "property-children")
 
 (prove::is
  (with-ontology f () 
@@ -94,7 +133,8 @@
 	 (sub-object-property-of !a !b)))
    (instantiate-reasoner f :hermit :config (quiet-reasoner-config))
    (property-descendants !b))
- (list !a))
+ (list !a)
+ "property-descendants")
 
 (prove:finalize) 
 
@@ -103,7 +143,6 @@
 (prove::is
  (all-owl2-term-alternatives 'class)
  '(class class "Class"))
-
 
 (prove::is
  (all-owl2-term-alternatives 'sub-class-of)
