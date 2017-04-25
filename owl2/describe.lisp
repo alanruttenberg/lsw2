46d1c02d787b656b817d90ed92a5573a2467a809
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-04-25 01:50:54 -0400

    Use *AFTER-SWANK-INIT-HOOK* to define inspect function, if swank is loaded

--- owl2/describe.lisp
+++ owl2/describe.lisp
@@ -16,31 +16,42 @@
 		       (get-referencing-axioms uri :individual *default-kb* t)))))
       (call-next-method)))
 
-#+swank
-(defmethod swank::emacs-inspect ((uri uri))
+(defun swank-emacs-inspect (uri)
   (if (and *default-kb*
 	   (some (lambda(type) (get-entity uri type *default-kb*))
 		 '(:class :object-property :data-property :individual)))
-      (append `((:label "URI: ") (:value ,uri ,(uri-full uri)) (:newline)
-		(:label "Ontology: ") (:value ,*default-kb*) (:newline))
-	      (loop for (prop value) in (entity-annotations uri)
-		    collect `(:label ,(or (uri-label prop )
-					  (uri-abbreviated prop)
-					  (uri-full prop)))
-		    collect ": "
-		    collect `(:value ,value)
-		    collect '(:newline))
-	      `((:label "Axioms") (:newline))
-	      (loop for type in '(:class :object-property :data-property :individual)
-		    for axioms = (when (get-entity uri type *default-kb*)
-				   (get-referencing-axioms uri :class *default-kb* t))
-		    when axioms append
-				(list* `(:label ,(format nil "As ~a: " (string-downcase type)))
-				       '(:newline)
-				       (loop for ax in axioms
-					     for pprinted = (with-output-to-string (s) 
-							      (let ((*print-lines* nil)
-								    (*print-right-margin* 100))
-								(pprint ax s)))
-					     collect `(:value ,ax ,(subseq pprinted 1))))))
+      (let ((*print-uri-with-labels-from* (or *print-uri-with-labels-from* (list *default-kb*)))
+	    (*print-uri-with-labels-show-source* nil))
+	(append `((:label "URI: ") (:value ,uri ,(uri-full uri)) (:newline)
+		  (:label "Ontology: ") (:value ,*default-kb*) (:newline))
+		(loop for (prop value) in (entity-annotations uri)
+		      collect `(:label ,(or (uri-label prop )
+					    (uri-abbreviated prop)
+					    (uri-full prop)))
+		      collect ": "
+		      collect `(:value ,value)
+		      collect '(:newline))
+		`((:label "Axioms") (:newline))
+		(loop for type in '(:class :object-property :data-property :individual)
+		      for axioms = (when (get-entity uri type *default-kb*)
+				     (get-referencing-axioms uri :class *default-kb* t))
+		      when axioms append
+				  (list* `(:label ,(format nil "As ~a: " (string-downcase type)))
+					 '(:newline)
+					 (loop for ax in axioms
+					       for pprinted = (with-output-to-string (s) 
+								(let ((*print-lines* nil)
+								      (*print-right-margin* 100))
+								  (pprint ax s)))
+					       collect `(:value ,ax ,(subseq pprinted 1)) collect '(:newline))))))
       (call-next-method)))
+
+(defun define-owl-emacs-inspect ()
+  (eval `(defmethod ,(intern "EMACS-INSPECT" "SWANK") ((uri uri))
+	   (swank-emacs-inspect uri))))
+
+(defvar cl-user::*after-swank-init-hook* nil)
+(pushnew 'define-owl-emacs-inspect cl-user::*after-swank-init-hook*)
+
+
+
