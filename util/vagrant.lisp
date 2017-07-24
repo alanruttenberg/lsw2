e7ba69b635d5c95eeddf03bb85cb4db8fcb0976d
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-07-24 00:07:36 -0400

    bugfix. add (get-vagrant-box-status id-or-name) (vagrant-up id-or-name)

--- util/vagrant.lisp
+++ util/vagrant.lisp
@@ -8,7 +8,7 @@
   (let ((debug-info (second (multiple-value-list (run-program-string->string "vagrant" (list "global-status" "--debug") "")))))
     (loop for match in 
 		    (all-matches debug-info
-				 " INFO interface: info: ([a-f0-9]+)\\s*INFO interface: info: (\\S+)\\s*INFO interface: info: (\\S+)\\s*INFO interface: info: (\\S+ )\\s*INFO interface: info: (\\S+)" 1 2 3 4 5)
+				 " INFO interface: info: ([a-f0-9]+)\\s*INFO interface: info: (\\S+)\\s*INFO interface: info: (\\S+)\\s*INFO interface: info: (\\S+)\\s*INFO interface: info: (\\S+)" 1 2 3 4 5)
 	  for (id name provider status wd) = match
 	  collect
 	  `(:id ,id :name ,name :provider ,provider :status ,status :wd ,wd))))
@@ -17,7 +17,16 @@
   (let ((info (get-vagrant-box-info)))
     (getf (find-if (lambda(e) (equalp (getf e :id) id-or-name) (equalp (getf e :name) id-or-name)) info) :wd)))
 
+(defun get-vagrant-box-status (id-or-name)
+  (let ((info (get-vagrant-box-info)))
+    (getf (find-if (lambda(e) (equalp (getf e :id) id-or-name) (equalp (getf e :name) id-or-name)) info) :status)))
 
+(defun vagrant-up (id)
+  (run-program-string->string  "vagrant" `("up" ,id) "")
+  (assert (equalp (get-vagrant-box-status id) "running") (id) "Failed to bring up vagrant box ~a" id))
+  
+  
+  
 (defun vagrant-tip ()
   (format t "Install or upgrade virtualbox: brew cask [re]install virtualbox~%")
   (format t "Download from https://www.vagrantup.com/downloads.html~%")
