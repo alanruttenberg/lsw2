ffe9edcc7c25329b6fc192b9751d20d2cfcca581
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-08-28 14:56:22 -0400

    bugfix reading dates, correctly get last column in the presence of empty cells

---
+++ read-ms-docs/mvn-bootstrap.sh
@@ -0,0 +1,4 @@
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=org.apache.poi:poi:3.15
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=org.apache.poi:poi-scratchpad:3.15
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=org.apache.poi:poi-ooxml:3.15
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=org.apache.poi:poi-ooxml-schemas:3.15
