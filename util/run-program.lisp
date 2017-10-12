cd10d4cf9905477cdee9b12eb9a7a5ef26e6c006
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-10-12 10:55:27 -0400

    handle case where there is no input

--- util/run-program.lisp
+++ util/run-program.lisp
@@ -1,9 +1,9 @@
 ;; Originally I read error first, and it would sometimes hang (on one vm but not another???)  So I switched it to
 ;; reading output first, and just in case, peek-char the error stream before doing a read-line
 (defun run-program-string->string (executable switches input &key wd)
-  (let ((process (sys::run-program executable switches :directory wd)))
-    (write-string input (sys::process-input process))
-    (close (sys::process-input process))
+  (let ((process (sys::run-program executable switches :directory wd :input (if input :stream))))
+    (when input (write-string input (sys::process-input process))
+	  (close (sys::process-input process)))
     (let ((output (with-output-to-string (s)
                     (loop for line = (read-line (sys::process-output process) nil :eof)
                           until (eq line :eof)
