7216ffccdf467347bab4279e1c646ca10590174a
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-11-15 00:32:32 -0500

    Add ability to use http instead of https to work around java 9 bug

--- util/send-prowl-notification.lisp
+++ util/send-prowl-notification.lisp
@@ -1,9 +1,10 @@
 (in-package :cl-user)
 
 (defvar *prowl-api-key* nil)
+(defvar *prowl-use-http* nil)
 
 (defun prowl-notify (title note &key (priority 3) (app "LSW") )
   (if *prowl-api-key*
-      (get-url "https://api.prowlapp.com/publicapi/add"
+      (get-url (format nil "~a://api.prowlapp.com/publicapi/add" (if *prowl-use-http* "http" "https"))
 	       :post `(("apikey" ,*prowl-api-key*) ("application" ,app) ("event" ,title) ("description" ,note) ("priority" ,(prin1-to-string (- priority 3)))))
       (format t "Notification from ~a: ~a - ~a ~%" app title note)))
