68fafde462ec8dd763820d807ba6ec7617df35fe
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-11-25 01:27:58 -0500

    when we've decided to cache an ontology, forget an old one, if cached

--- owl2/package.lisp
+++ owl2/package.lisp
@@ -6,7 +6,7 @@
 ;; First attempts to isolate something in LSW using packages
 (defpackage lsw2cache
   (:use cl jss ext)
-  (:import-from :cl-user #:get-url #:uri-full #:uri-p #:find-elements-with-tag #:attribute-named #:load-ontology #:loaded-documents #:*use-cache-aware-load-ontology*)
+  (:import-from :cl-user #:get-url #:uri-full #:uri-p #:find-elements-with-tag #:attribute-named #:load-ontology #:loaded-documents #:*use-cache-aware-load-ontology* #:forget-cached-url)
   (:export #:cache-ontology-and-imports #:uncache-ontology))
 
 (shadowing-import '(lsw2cache::cache-ontology-and-imports lsw2cache::uncache-ontology lsw2cache::ontology-cache-location lsw2cache::cache-ontology-and-imports lsw2cache::uri-mapper-for-source) 'cl-user) 
