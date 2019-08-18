f0f56226a7b0ba309e9b1e39ab8e26db24843e82
Alan Ruttenberg <alanruttenberg@gmail.com>
2019-08-18 01:05:31 -0400

    some refactoring of jdbc.lisp . Change connection argument to sql-query from &optional to &key.  First draft sexp-sql.lisp for a lispy way of writing sql. Still needs refinement + documentation

--- util/jdbc.lisp
+++ util/jdbc.lisp
@@ -1,3 +1,5 @@
+(in-package :cl-user)
+
 ;; Copyright © 2013 Alan Ruttenberg and SUNY at Buffalo All Rights Reserved.
 
 ;; Redistribution and use in source and binary forms, with or without
@@ -31,6 +33,11 @@
 (defvar *default-connection* nil)
 (defvar *jdbc-trace* nil)
 
+;; if requested in call to sql-query save the query results, indexed by the SQL+JDBC URL. We can get the JDBC URL from
+;; the connection using (#"getURL")
+
+(defvar *jdbc-query-cache* (make-hash-table :test 'equalp))
+
 (defun with-jdbc-connection (fn jdbc-url)
   (let ((connection nil))
     (unwind-protect
@@ -44,6 +51,9 @@
       (when (and connection (not (equal (car *default-connection*) jdbc-url)))
 	(#"close" connection)))))
 
+(defun set-default-jdbc-connection (jdbc-url)
+  (setq *default-connection* (cons jdbc-url (#"getConnection" 'java.sql.DriverManager jdbc-url))))
+
 (defgeneric with-jdbc-connection-named (keyword function)
   (:documentation "if a sql query is executed with a connection that is a keyword instead of a connection object, the sql is wrapped in a lambda and passed to this function, which calls the lambda with a single argument which is the connection. Clients of this functionality need to define eql methods on the first argument to implement the necessary setup of the connection"
   ))
@@ -54,56 +64,148 @@
 	      (mapcar (lambda(e) (if (stringp e) e (apply 'format nil e))) query))
       query))
     
+(defun check-if-known-database (connection)
+  (assert   (or (equal "com.microsoft.sqlserver.jdbc.SQLServerConnection" (jclass-name (jobject-class connection)))
+		(equal "oracle.jdbc.driver.T4CConnection" (jclass-name (jobject-class connection)))
+		(equal "com.hxtt.sql.access.r" (jclass-name (jobject-class connection)))
+		(equal "org.postgresql.jdbc4.Jdbc4Connection" (jclass-name (jobject-class connection)))
+		(equal "org.sqlite.Conn" (jclass-name (jobject-class connection)))
+		(equal "com.mysql.jdbc.JDBC4Connection" (jclass-name (jobject-class connection)))
+		(equal "sybase.jdbc4.sqlanywhere.IConnection" (jclass-name (jobject-class connection))))
+	    (connection) "Don't yet support sql-query for ~a" (jclass-name (jobject-class connection))))
+   
+(defun maybe-format-sql (query &optional format-args)
+  (if (stringp query) (Setq query (list query)))
+  (cond ((and (listp query)
+	      (stringp (car query)))
+	 (setq query (format nil "~{~a~^ ~}" 
+			(mapcar (lambda(e) 
+				  (if (stringp e) e (apply 'format nil e)))
+				query)))
+	 (when format-args (setq query (apply 'format nil query format-args)))
+	 query)
+	((listp query)
+	 (eval query))))
+
+
+(defun maybe-default-connection (connection)
+  (assert (or connection *default-connection*) (connection) "Either connection or *default-connection* needs to be non-nil")
+  (or connection (cdr *default-connection*)))
+
+
 ;; Do a sql query to connection. Result is a list of list, with each list one row of fields.
 ;; if with-headers is non-nil, then the values are instead ("fieldname" . value) instead of just value.
 ;; query can be a list of strings, in which case they are concatenated
 ;; if format-args is supplied then the query string used as a format string with the format-args
-;; multiple values are returned: 1. the result of the query, 2. the field names of the resultset
-(defun sql-query (query &optional (connection (cdr *default-connection*)) &key with-headers print format-args trace)
-  (when (listp query) (setq query (format nil "~{~a~^ ~}" 
-					  (mapcar (lambda(e) (if (stringp e) e (apply 'format nil e))) query))))
-  (when format-args (setq query (apply 'format nil query format-args)))
-  (when (or trace *jdbc-trace*) (print query))
-  (if (keywordp connection)
-      (with-jdbc-connection-named connection
-	(lambda(c) (sql-query query c :with-headers with-headers :print print)))
-      (if (and (null connection) (not (null *default-connection*)))
-	  (sql-query query (cdr *default-connection*) :with-headers with-headers :print print)
-	  (cond ((or (equal "com.microsoft.sqlserver.jdbc.SQLServerConnection" (jclass-name (jobject-class connection)))
-		     (equal "oracle.jdbc.driver.T4CConnection" (jclass-name (jobject-class connection)))
-		     (equal "com.hxtt.sql.access.r" (jclass-name (jobject-class connection)))
-		     (equal "org.postgresql.jdbc4.Jdbc4Connection" (jclass-name (jobject-class connection)))
-		     (equal "org.sqlite.Conn" (jclass-name (jobject-class connection)))
-		     (equal "com.mysql.jdbc.JDBC4Connection" (jclass-name (jobject-class connection)))
-		     (equal "sybase.jdbc4.sqlanywhere.IConnection" (jclass-name (jobject-class connection))))
-		 (let (statement results)
-		   (unwind-protect 
-			(progn
-			  (setq statement (#"createStatement" connection))
-			  (setq results (#"executeQuery" statement (if (consp query) (apply 'format nil (car query) (cdr query)) query )))
-			  (when print 
-			    (format t "~{~a~^	~}~%" (loop for i from 1 to (#"getColumnCount" (#"getMetaData" results)) collect (#"getColumnName" (#"getMetaData" results) i))))
-			  (let ((field-names
-				 (loop 
-				    for i from 1 to (#"getColumnCount" (#"getMetaData" results))
-				    collect (#"getColumnName" (#"getMetaData" results) i)))) ; collect field names
-			    (loop 
-			       while (#"next" results) 
-			       with columns
-			       collect (block columns
-					 (loop 
-					    for field in field-names
-					    if with-headers
-					    collect (cons field (#"getString" results field)) into columns 
-					    else collect (#"getString" results field) into columns
-					    finally (return-from columns (if print (progn (format t "~{~s~^	~}~%" (mapcar 'cdr columns)) (values)) columns))))
-			       into rows
-			       finally (if print nil (return-from nil (values rows field-names))))))
-			  (and (boundp 'results) results (#"close" results))
-			  (and (boundp 'statement) statement (#"close" statement)))
-		   ))
-		(t (error "Don't yet support sql-query for ~a" (jclass-name (jobject-class connection))))))))
+;; if print is non-nil then the rows are printed in a tabular form and there is no return value
+;; otherwise two values are returned:
+;;   1. the result of the query,
+;;   2. the field names of the resultset
 
+(defvar *last-sql-query* nil)
+(defvar *last-sql-query-results* nil)
+(defvar *always-cache-sql* nil)
+
+(defun sql-query (query &rest args &key (connection (cdr *default-connection*)) with-headers print format-args trace (dwim-capitalized t) cache save-to)
+  (if (keywordp connection) 
+      (return-from sql-query 
+	(with-jdbc-connection-named connection
+	  (lambda(c) (apply 'sql-query query c args))))
+      (setq connection (maybe-default-connection connection)))
+  (setq query (maybe-format-sql query format-args))
+  (when (or trace *jdbc-trace*) (print query))
+  (check-if-known-database connection)
+  (if (and dwim-capitalized (equal "oracle.jdbc.driver.T4CConnection" (jclass-name (jobject-class connection))))
+      (setq query (oracle-quote-capitalized query)))
+  ;; Now we have the query
+  (setq *last-sql-query* query)
+
+  ;; maybe return cached query, otherwise clear cache for this query
+  ;; BUGS:
+  ;;  - The print and with-headers args are not handled. If you change them the cache will be wrong
+  ;;  - Don't distinguish no cache hit vs result nil
+  (if (or cache *always-cache-sql*) 
+      (let* ((cache-key (cons (#"getURL" connection) query))
+	     (cache-value (gethash cache-key *jdbc-query-cache*)))
+	(when (and cache cache-value)
+	  (setq *last-sql-query-results* (car cache-value))
+	  (return-from sql-query (values-list cache-value))))
+      (remhash  (cons (#"getURL" connection) query) *jdbc-query-cache*))
+
+  ;; otherswise execute the query
+  (if save-to
+      (with-open-file (f save-to :if-exists :supersede :if-does-not-exist :create :direction :output)
+	(let ((*standard-output* f))
+	  (print-or-return-query-results query connection t with-headers (or cache *always-cache-sql*))))
+      (print-or-return-query-results query connection print with-headers (or cache *always-cache-sql*)))
+  )
+
+(defun print-or-return-query-results (query connection print with-headers cache &optional file)
+  (let ((statement :s)
+	(results :r))
+    (unwind-protect 
+	 (progn
+	   (setq statement (#"createStatement" connection))
+	   (setq results (#"executeQuery" statement query))
+	   (let ((field-names
+		   (loop 
+		     for i from 1 to (#"getColumnCount" (#"getMetaData" results))
+		     collect (#"getColumnName" (#"getMetaData" results) i))))
+	     (when print 
+	       (format t "~{~a~^	~}~%" field-names))
+	     (loop
+	       while (#"next" results) 
+	       collect
+	       (loop 
+		 for field in field-names
+		 if with-headers
+		   collect (cons field (#"getString" results field)) into columns 
+		 else collect (#"getString" results field) into columns
+						
+		 finally (progn
+			   (when print
+			     (format t "~{~a~^	~}~%"
+				     (if with-headers
+					 (mapcar 'cdr columns)
+					 columns 
+					 )
+				     ))
+			   (return columns)
+			   ))
+		 into rows
+	       finally
+		  (progn
+		    (setq *last-sql-query-results* rows)
+		    (when cache (setf (gethash (cons (#"getURL" connection) query) *jdbc-query-cache*) (list rows field-names)))
+		    (return (if print
+				nil
+				(values rows field-names)))))))
+      (and (not (eq results :r)) results (#"close" results))
+      (and (not (eq statement :s)) statement (#"close" statement)))
+    ))
+
+;; Oracle is annoying. Table and column names are case insensitive unless surrounded by double quote.
+;; This takes a string and adds those quotes, effectively making the queries case sensitive .
+(defun oracle-quote-capitalized (string)
+  (if (find #\" string)
+      string
+      (progn
+	(setq string (#"replaceAll" string "([$])" "\\\\$1" ))
+	(let* ((replacements nil)
+	       (count 0)
+	       (protected 
+		 (replace-all string "('\\S+')" (lambda(quoted)
+						  (let ((token (format nil "'@@~a@@'" (incf count))))
+						    (push (list token quoted) replacements)
+						    token))
+			      1))
+	       ;; fixed: A capitalized string can have more than one capital letter
+	       (capitalized (#"replaceAll" protected "([A-Z]+[a-z_][A-Za-z_]*)" "\"$1\""))
+	       (reconstituted (replace-all capitalized "('@@\\d+@@')" (lambda(token) (second (assoc token replacements :test 'equalp))) 1)))
+
+;	  (print-db protected capitalized reconstituted)
+	  reconstituted			;(#"replaceAll" reconstituted "([$])" "\\\\$1" ) ;; also quote $ signs
+	  ))))
 
 (defun sql-server-driver-properties ()
   (map 'list (lambda(e)
@@ -117,18 +219,18 @@
 ;; not enough in java 7 - put sqljdbc4.jar in same dir as abcl.jar
 
 (defun table-column-names  (table connection)
-  (mapcar 'car (car (sql-query (list "select top 1 * from ~a" table) connection :with-headers t))))
+  (mapcar 'car (car (sql-query (list "select top 1 * from ~a" table) :connection connection :with-headers t))))
 
 (defun table-rowcount (table connection)
   "Return the number or rows in a table"
-  (caar (sql-query (list "select count(*) from ~a" table) connection)))
+  (caar (sql-query (list "select count(*) from ~a" table) :connection connection)))
 
 (defun sql-server-columns-matching (connection &key (table-match "%")  schema (column-match "%"))
   "Retrieve table,schema,column for all tables. Constraint tables to match table-match and column to match column-match"
   (sql-query (list "SELECT t.name AS table_name, SCHEMA_NAME(schema_id) AS schema_name, c.name AS column_name FROM sys.tables AS t
 INNER JOIN sys.columns c ON t.OBJECT_ID = c.OBJECT_ID
 where t.name like '~a' and c.name like '~a'
-ORDER BY schema_name, table_name;" table-match  column-match) connection))
+ORDER BY schema_name, table_name;" table-match  column-match) :connection connection))
 
 
 ;; let's assume this works for all connections until we know otherwise
@@ -214,14 +316,13 @@ ORDER BY schema_name, table_name;" table-match  column-match) connection))
      (get-all-tables-and-columns connection))
     them))
 
-
 (defun table-counts (connection tables &optional (threshold 1) &rest fields)
   (loop for table in tables 
-     for count = (parse-integer (caar (sql-query (format nil "select count(*) from ~a" table) connection)))
+     for count = (parse-integer (caar (sql-query (format nil "select count(*) from ~a" table) :connection connection)))
      if (and (plusp count)
 	     (<= count threshold)
 	     fields)
-     collect (list table count (sql-query (format nil "select ~{~a~^,~} from ~a" fields table) connection))
+     collect (list table count (sql-query (format nil "select ~{~a~^,~} from ~a" fields table) :connection connection))
      else collect (list table count)))
 
 (defun describe-table (connection table)
