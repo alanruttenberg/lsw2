18948dca989f4a5456d3b210a5b3e65ed3598c44
Alan Ruttenberg <alanruttenberg@gmail.com>
2019-10-30 16:21:57 -0400

    import symbols from cl-unification

--- logic/package.lisp
+++ logic/package.lisp
@@ -20,6 +20,9 @@
 		cl-user::tree-replace cl-user::tree-find cl-user::tree-walk cl-user::tree-remove-if cl-user::tree-walk-conditional  cl-user::?
 		cl-user::all-matches
 		)
+  ;; cl-unification
+  (:import-from :it.unimib.disco.ma.cl.ext.dacf.unification "UNIFY" "FIND-VARIABLE-VALUE" "MATCH-CASE" "MATCH")
+
 ;  (:import-from :winston-ai  #:remember-assertion #:remember-rule #:forward-chain #:make-empty-stream #:*rules* #:*assertions*)
   (:import-from :ginsberg-cnf-dnf ginsberg-cnf-dnf::<= ginsberg-cnf-dnf::=> ginsberg-cnf-dnf::<=>) 
   (:import-from :sys sys::run-program sys::process-output sys::process-input)
