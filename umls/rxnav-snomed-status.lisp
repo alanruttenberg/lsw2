cccd13448bb6d655cae369f1ea1f6e3197e16827
Alan Ruttenberg <alanruttenberg@gmail.com>
2019-10-30 15:59:00 -0400

    Api.lisp Added crosswalk api (mapping)  (umls-mapped-identifiers-for-source "HPO" "HP:0001947" :targetsource "SNOMEDCT_US") Added definitions api (umls-concept-definitions "C0025990")
    New file icd.lisp
     New functions:
     (snomeds-for-cui cui)
     (imagic-snomed-to-icd snomed-id snomed-description)
    
    define-api-function:
      fix caching code
      get ticket granting ticket if we don't already have one

---
+++ umls/rxnav-snomed-status.lisp
@@ -0,0 +1,19 @@
+;; https://rxnav.nlm.nih.gov/SnomedCTAPI.html
+
+(defun get-snomed-id-status (id)
+  (let ((info (xmls::parse (get-url (format nil "https://rxnav.nlm.nih.gov/REST/SnomedCT/status?id=~a" id)))))
+    (let ((status
+	    (cdr (assoc (third (first (find-immediate-children-with-tag info "status")))
+			'(("-1" . :unknown)
+			  ("0" . :retired)
+			  ("1" . :active)
+			  ("2" . :moved))
+			:test 'equalp))))
+      (if (eq status :moved)
+	  (list* :moved
+		 (mapcar 'third (find-immediate-children-with-tag info "mappedId")))
+	  status))))
+	  
+
+
+
