693e874d3607943779ffcbab46ea3a350f64b1c3
Alan Ruttenberg <alanruttenberg@gmail.com>
2019-11-04 23:36:21 -0500

    WIP - added scraped i-magic for mapping snomed to icd. new functions cuis-for-snomed-id definitions-for-snomed-id snomed-ids-for-label

--- umls/icd.lisp
+++ umls/icd.lisp
@@ -6,11 +6,11 @@
 	  (remove "ICD10" (umls-concept-atoms cui :language "ENG") :key (lambda(e) (cdr (assoc :root-source e))) :test-not 'equalp)))
 
 (defun snomeds-for-cui (cui)
-  (mapcan (lambda(e) (let ((found (cdr (assoc :code e))))
+  (remove-duplicates (mapcan (lambda(e) (let ((found (cdr (assoc :code e))))
 		       (if found
 			   (list (#"replaceFirst" found ".*/" ""))
 			   nil)))
-	  (remove "SNOMEDCT_US" (umls-concept-atoms cui :language "ENG") :key (lambda(e) (cdr (assoc :root-source e))) :test-not 'equalp)))
+	  (remove "SNOMEDCT_US" (umls-concept-atoms cui :language "ENG") :key (lambda(e) (cdr (assoc :root-source e))) :test-not 'equalp)) :test 'equalp))
 
 (defun icd-cuis (icd)
   (mapcan (lambda(e)
@@ -29,6 +29,117 @@
   
 (defun mapped-icd (from id)
   (ignore-errors (extract-icds (umls-mapped-identifiers-for-source from (princ-to-string id) :targetsource "ICD10"))))
+
+(defun cuis-for-snomed-id (id)
+  (let ((atoms (umls-source-term-atoms  "SNOMEDCT_US"  id)))
+    (remove-duplicates
+     (remove nil
+	    (mapcar (lambda(entry)
+	      (let ((found (cdr (assoc :concept entry))))
+		(when found
+		  (#"replaceAll" found ".*/" ""))))
+		    atoms)) :test 'equalp)))
+
+
+(defun definitions-for-snomed-id (id)
+  (let ((cuis (cuis-for-snomed-id id))
+	(them nil))
+    (if (> (length cuis) 1)
+	(values nil cuis)
+	(mapcar (lambda(e)
+		  (let ((source (cdr (assoc :root-source e))))
+		    (unless (or (member source '("SCTSPA") :test 'equalp)
+				(#"matches" source "MSH.?.*"))
+		      (push (cons (cdr (assoc :value e)) source) them)
+			    (cdr (assoc :value e)))))
+		(car (umls-concept-definitions (car cuis) :ignore-errors t))))
+    them))
+
+;; snomed-ids-for-label
+;;
+;; Input is a label, purportedly of a SNOMED term.
+;; Output is a list of pairs. The first element is a SNOMED id, the second
+;; element a list of synonyms.
+;;
+;; (snomed-ids-for-label "Cleft hard and soft palate with bilateral cleft lip")
+;; ->
+;; (("1085331000119107"
+;;   "Cleft hard and soft palate and bilateral cleft lip"
+;;   "Cleft palate and bilateral cleft lip"
+;;   "Cleft palate and bilateral cleft lip (disorder)"
+;;   "Uranostaphyloschisis with bilateral cleft lip")
+;;  ("762586009"
+;;   "Cleft hard and soft palate with bilateral cleft lip and alveolus"
+;;   "Cleft hard and soft palate with bilateral cleft lip and alveolus (disorder)"))
+;;
+;; Strategy
+;; We're looking through search results for label within SNOMED
+;; Maybe there's an exact match, maybe not.
+;; If there's an exact match, then we have a cui, which one might think uniquely identifies a SNOMED term, but nope
+;; We ask for the SNOMED ids for that CUI (which might have similar terms) and for each CUI returned
+;; If the match is unambiguous then only that is returned, otherwise there can be multiple ids/synonyms and
+;; it has to be manually disambiguated.
+
+(defun snomed-ids-for-label (label)
+  (let* ((results (umls-search :string label :sabs "SNOMEDCT_US" ))
+	 (name-cuis (loop for result in (cdr (assoc :results results))
+			  for name = (cdr (assoc :name result))
+			  for cui =  (cdr (assoc :ui result))
+			  unless (equalp cui "NONE")
+			    collect (list name cui))))
+    (let* ((lookups 
+	    (if  (find label name-cuis :test 'equalp :key 'car)
+		 (snomed-id-for-cui (second (find label name-cuis :test 'equalp :key 'car)))
+		  (mapcan (lambda (e)
+			    (snomed-id-for-cui (second e) ))
+			  name-cuis)))
+	   (exact (find label lookups :key 'car :test 'equalp)))
+      (let ((maybe 
+	      (if exact
+		  (let ((id (second exact)))
+		    (list (list* id (mapcar 'car (remove (second exact) lookups :test-not 'equalp :key 'second)))))
+		  (loop for id in (remove-duplicates (mapcar 'second lookups)  :test 'equalp)
+			collect (list* id (mapcar 'car (remove id lookups :test-not 'equalp :key 'second)))))))
+	(or maybe
+	    (and (#"matches" label ".*\\(.*")
+		 (snomed-ids-for-label (#"replaceAll" label "\\s*\\(.*" ""))))))))
+	
+	    
+	    
+;; snomed-id-for-cui
+;; 
+;; Input is a CUI
+;; Output is a list of pairs of id and label.
+
+;; (snomed-id-for-cui "C0206067" )
+;; ->
+;; (("Focal epithelial hyperplasia" "36949004")
+;;  ("Focal epithelial hyperplasia (morphologic abnormality)" "36949004")
+;;  ("Focal epithelial hyperplasia of mouth" "6121001")
+;;  ("Focal epithelial hyperplasia of mouth (disorder)" "6121001")
+;;  ("Heck's disease" "6121001")
+;;  ("Multifocal epithelial hyperplasia" "6121001")
+;;  ("Oral focal epithelial hyperplasia" "6121001"))
+
+;; Worthy of note
+;; - A CUI can be associated with multiple SNOMED terms
+;; - A SNOMED term can have multiple synonyms.
+;; - The labels have annotations to the effect of whether they are full names,
+;; - preferred names, etc. We don't return that information as it isn't important
+;; for this application.
+
+(defun snomed-id-for-cui (cui)
+  (let* ((entries
+	    (umls-concept-atoms cui :sabs "SNOMEDCT_US"))
+	 (name-sctids
+	   (loop for result in entries
+		 for name = (cdr (assoc :name result))
+		 for sctid =  (#"replaceAll" (cdr (assoc :code result)) ".*/" "")
+		 collect (list name sctid))))
+    name-sctids))
+
+(defun snomed-labels-for-id (id)
+  (remove-duplicates (mapcar (lambda(e) (cdr (assoc :name e))) (umls-source-term-atoms  "SNOMEDCT_US" id))    :test 'equalp))
 
 #|
 Retrieval of icd codes mapped from SNOMED using i-magic web form.
@@ -105,7 +216,7 @@ The web demo https://imagic.nlm.nih.gov/imagic/code/map
 						  (#"encode" 'java.net.URLEncoder
 							     (coerce (second el) 'simple-string) "UTF-8")))
 					  parameters)))))
-	  (let ((response (get-url url :force-refetch t)))
+	  (let ((response (get-url url )))
 	    (if debug
 		(progn
 		  (setq @ response)
@@ -123,7 +234,40 @@ The web demo https://imagic.nlm.nih.gov/imagic/code/map
 		)))))))
 
 (defun map-refinements? (response)
-  (if (all-matches response ">Problem refinement</th>" 0)
-      (let ((choices-text (all-matches response "(?s)<p>Would one of the following diagnoses apply\\?  Choose the most specific one:</p>(.*?)submit-bu
-ttons" 0)))
-	(remove "" (mapcar 'car (all-matches (caar choices-text) "<input type=\"radio\" [^>]*?><label [^>]*?>([^<]*)" 1)) :test 'equalp))))
+  (append
+   (when (all-matches response ">Problem refinement</th>" 0)
+       (let ((choices-text (all-matches response "(?s)<p>Would one of the following diagnoses apply\\?  Choose the most specific one:</p>(.*?)<td>" 0)))
+	 (remove "" (mapcar 'car (all-matches (caar choices-text) "<input type=\"radio\" [^>]*?><label [^>]*?>([^<]*)" 1)) :test 'equalp)))
+   (when (all-matches ">Required Episode refinement<" 0)
+       (let ((choices-text (all-matches response 
+
+			)))))))
+
+(defun map-refinements? (response)
+  (let ((refinement-sections (all-matches response "((?s)<tr[^>]*>(.*?)</tr>)" 1)))
+    (loop for (section) in refinement-sections
+	  for icd =  (caar (all-matches section "(?s)<span class=\"icdcode\">(.*?)</span>" 1))
+	  for episode? = (all-matches section "(?s)(Required Episode refinement)" 1)
+	  for options = (let ((choices-text (all-matches section "(?s)<p>Would one of the following diagnoses apply\\?  Choose the most specific one:</p>(.*?)</td>" 0)))
+			  (when choices-text
+			    (remove "NoneOfTheAbove"
+				    (all-matches (caar choices-text)
+						 "<input type=\"radio\"\\s*value=\"([^\"]*)\"[^>]*?><label [^>]*?>([^<]*)" 1 2)
+				    :key 'car :test 'equalp)))
+	  for episodes = (all-matches section
+				      "(?s)<div class=\"option\"><input.*?value=\"([^\"]*)\".*?(<span.*?</div>)"
+				      1 2)
+	  when (or episode? options episodes icd)
+	    collect
+	    (if (or episode? options)
+		(or episode? options)
+		(if icd
+		    (list :icd icd
+			  (caar (all-matches section "(?s)icdname\">([^>]*?)<" 1)))
+			  
+		    (loop for (value gunk) in  episodes
+			  collect (list value (#"replaceAll" gunk "<.*?>" ""))))))))
+
+
+
+;(all-matches h "<div class=\"option\"><input type=\"radio\" value=\"[^\"]*\".*(<span>.*<div>)"  1 2) 
