39c62c3350c8921d76b2ec2682fa6d9819a33479
Alan Ruttenberg <alanruttenberg@gmail.com>
2020-03-12 16:22:06 -0400

    Allow specification of indent prefix for text class trees

--- owl2/text-classtree.lisp
+++ owl2/text-classtree.lisp
@@ -3,27 +3,29 @@
 (defclass text-tree-node ()
   ((term :initarg :term :reader text-tree-node-term)
    (ontology :initarg :ontology :accessor text-tree-node-ontology)
-   (children :initarg :children :accessor text-tree-node-children :initform nil)))
+   (children :initarg :children :accessor text-tree-node-children :initform nil)
+   (indent-string :initarg :indent-string :accessor indent-string :initform (string #\space ))
+   (indent-increment :initarg :indent-increment :accessor indent-increment :initform 2 )))
 
-(defun create-text-tree-structure (ontology class &optional parent)
-  (let ((node (make-instance 'text-tree-node :term class :ontology ontology)))
+(defun create-text-tree-structure (ontology class &optional parent (indent-string (string #\space)) (indent-increment 1))
+  (let ((node (make-instance 'text-tree-node :term class :ontology ontology :indent-string indent-string
+			     :indent-increment indent-increment)))
     (when parent 
       (pushnew node (text-tree-node-children parent) :key 'text-tree-node-term))
     (when (children class ontology)
       (loop for child in (children class ontology)
-	 do (create-text-tree-structure ontology child node)))
+	 do (create-text-tree-structure ontology child node indent-string)))
     node))
 
 (defmethod print-text-tree ((node text-tree-node) &optional (indent 0))
   (print-one-node node indent)
   (if (text-tree-node-children node)
-      (loop for child in (text-tree-node-children node) do (print-text-tree child (+ indent 2)))))
+      (loop for child in (text-tree-node-children node) do (print-text-tree child (+ indent (indent-increment node))))))
 
 (defmethod print-one-node  ((node text-tree-node) &optional (indent 0))
-  (loop repeat indent do (write-char #\space))
+  (loop repeat indent do (write-string (indent-string node)))
   (write-string (car (rdfs-label (text-tree-node-term node) (text-tree-node-ontology node))))
   (terpri))
-
   
 (defun test ()
   (with-ontology test (:collecting t)
