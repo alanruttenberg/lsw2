efab56c60659d58cdb9c83784f22ab640e75e0bc
Alan Ruttenberg <alanruttenberg@gmail.com>
2020-03-12 16:52:01 -0400

    Bind standard streams for process we use use to monitor timeout

--- util/eval-with-timeout.lisp
+++ util/eval-with-timeout.lisp
@@ -1,21 +1,25 @@
-(defun eval-with-timeout (expression time &optional when-done &aux done it result)
-  (unwind-protect
-       (progn 
-	 (setq it (threads:make-thread 
-		   (lambda()
-		     (catch 'abort
-		       (flet ((doit ()
-				(eval expression)))
-			 (prog1
-			     (setq result 
-				   (if when-done
-				       (funcall when-done (multiple-value-list (doit)))
-				       (multiple-value-list (doit))))
-			   (setq done t)))))))
-	 (loop repeat (* 10 time) until done do (sleep .1)))
-    (unless done
-      (threads::destroy-thread it))
-    (unless done
-      (abort))
-    )
-  (values-list result))
+(defmacro eval-with-timeout (expression time &optional when-done )
+  `(let (done it result)
+     (unwind-protect
+	  (progn 
+	    (setq it (threads:make-thread 
+		      (let ((*standard-output* *standard-output*)
+			    (*debug-output* *debug-io*)
+			    (*trace-output* *trace-output*))
+			(lambda()
+			    (catch 'abort
+			      (flet ((doit ()
+				       (progn ,expression)))
+				(prog1
+				    (setq result 
+					  (if ,when-done
+					      (funcall ,when-done (multiple-value-list (doit)))
+					      (multiple-value-list (doit))))
+				  (setq done t))))))))
+	    (loop repeat (* 10 ,time) until done do (sleep .1)))
+       (unless done
+	 (threads::destroy-thread it))
+       (unless done
+	 (abort))
+       )
+     (values-list result)))
