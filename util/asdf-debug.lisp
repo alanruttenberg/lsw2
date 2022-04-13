a25f174def6ed57c2bc961c4e90c0e63094f5b01
Alan Ruttenberg <alanruttenberg@gmail.com>
2022-04-13 17:59:33 -0400

    New keyword :relative. If t(default) removes my and quicklisp's repo directories, for less clutter

--- util/asdf-debug.lisp
+++ util/asdf-debug.lisp
@@ -2,7 +2,7 @@
 
 ;; Print out a semi-readable list of the actions that load-system would do.
 
-(defun show-system-plan (system-name &key force)
+(defun show-system-plan (system-name &key force (relative t))
   (let* ((system (asdf::find-system system-name))
 	 (plan (asdf::make-plan 'asdf::sequential-plan 'asdf::load-op system 
 	       :forcing (if force 
@@ -16,8 +16,10 @@
 		 (cond ((and (member op '(:load-op :compile-op))
 			     (member kind `(:cl-source-file :org)))
 			(format t "~a ~a~%" op
-				(#"replaceFirst"
-				 (#"replaceFirst" (namestring (asdf::component-pathname (cdr action))) "/Users/alanr/repos/" "")
-				 "/Users/alanr/quicklisp/dists/quicklisp/software/" "")
+				(if relative 
+				    (#"replaceFirst"
+				     (#"replaceFirst" (namestring (asdf::component-pathname (cdr action))) "/Users/alanr/repos/" "")
+				     "/Users/alanr/quicklisp/dists/quicklisp/software/" "")
+				    (namestring (asdf::component-pathname (cdr action))))
 			       (asdf::component-name (cdr action)))))))
 	 (subseq (asdf::plan-actions plan) 0 ))))
