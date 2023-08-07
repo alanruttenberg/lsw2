31ccb28d732f163ed3afbf20760e6378367fd4b4
Alan Ruttenberg <alanruttenberg@gmail.com>
2023-08-07 18:58:05 -0400

    By default, append a label for the source ontology for terms brought into the module that aren't in the original signature

--- owl2/module.lisp
+++ owl2/module.lisp
@@ -2,57 +2,130 @@
 
 ;; http://owlapi.sourceforge.net/javadoc/uk/ac/manchester/cs/owlapi/modularity/SyntacticLocalityModuleExtractor.html
 
-(defun terms-in-ontology (kb uri)
-  (let (ont)
-    (mapcar (lambda(e)
-	      (when (equal (#"toString" (#"getOntologyIRI" (#"getOntologyID"  e))) (uri-full uri))
-		(setq ont e)))
-	    (set-to-list (#"getImportsClosure" (v3kb-ont kb))))
-    ont))
+;; Takes as input an ontology or the pathname of uri of an ontology,
+;; loading if the latter.  Computes the signature, not including any
+;; imports, then creates a module (an ontology) with that signature A
+;; module is a reduction of an ontology such that any query involving a
+;; term in the signature will have the same answer as if done against
+;; the full ontology. All the annotations of terms in the module are
+;; copied from the original ontology.
 
-(defun trimmed-imports-module (ontology &key  (module-uri (#"get" (#"getOntologyIRI" (#"getOntologyID" (v3kb-ont ontology)))))
-			       dest (module-type "STAR")
-			       (include-object-properties t)
-			       (include-data-properties t)
-			       (include-annotation-properties t)
-			       )
-  (let* ((manager (#"createOWLOntologyManager" 'org.semanticweb.owlapi.apibinding.OWLManager))
-	 (extractor (new 'SyntacticLocalityModuleExtractor manager (v3kb-ont ontology)   (get-java-field 'modularity.ModuleType module-type)))
-	 (sig (let ((it (#"getClassesInSignature" (v3kb-ont ontology))))
+;; If dest is a pathname, writes the new ontology to dest.
+;; If dest is t and the source of the ontology is the local file system, writes the
+;; ontology module to the file with "-module" appended in the same directory
+
+;; Module type can be "STAR", "TOP", and "BOTTOM". I've found STAR to be
+;; most useful.
+
+;; By default all classes, object properties and data properties are
+;; included in the signature. use :include-data-properties or
+;; :include-object-properties as nil to disable either.
+
+;; If module-iri is past an iri then the module ontology iri is set to
+;; that, otherwise its the same as the original.
+
+;; By default, any terms that are brought into the module but not in the original
+;; are labeled by their source (as heuristically determined based on their IRI)
+;; Use :label-imported nil to override that behavior
+
+(defun trimmed-imports-module (ontology &key (module-iri )
+			                  dest (module-type "STAR")
+			                  (include-object-properties t)
+			                  (include-data-properties t)
+                                          (label-imported t)
+			                  )
+  (when (stringp ontology)
+    (setq ontology (load-ontology ontology)))
+  (when (eq dest t)
+    (let ((source (ignore-errors (probe-file (v3kb-source ontology)))))
+      (if (and source (pathname-host source))
+          (error "can't default dest because source isn't a pathname")
+          (setq dest (merge-pathnames (make-pathname :name (format nil "~a-module" (pathname-name source))) source)))))
+  (let* ((sig (let ((it (#"getClassesInSignature" (v3kb-ont ontology))))
 		(and include-object-properties (#"addAll" it (#"getObjectPropertiesInSignature" (v3kb-ont ontology))))
 		(and include-data-properties (#"addAll" it (#"getDataPropertiesInSignature" (v3kb-ont ontology))))
-		it))
-	 (extracted (#"extractAsOntology" extractor sig (to-iri module-uri)))
+		it)))
+    (let ((result (create-module-for-signature ontology sig :dest dest :module-iri module-iri :module-type module-type
+                                               :label-imported label-imported)))
+      (when dest
+        (setf (v3kb-source result) dest))
+      result)))
+
+
+(defun create-module-for-signature (ontology signature &key module-iri dest (module-type "STAR") (label-imported t))
+  ;; Default module-iri to same as original 
+  (unless module-iri
+    (setq module-iri (#"get" (#"getOntologyIRI" (#"getOntologyID" (v3kb-ont ontology))))))
+
+  ;; Do the module extraction
+  (let* ((manager (#"createOWLOntologyManager" 'org.semanticweb.owlapi.apibinding.OWLManager))
+	 (extractor (new 'SyntacticLocalityModuleExtractor manager (v3kb-ont ontology)
+                         (get-java-field 'modularity.ModuleType module-type)))
+	 (extracted (#"extractAsOntology" extractor signature (to-iri module-iri)))
 	 (changes (new 'arraylist))
 	 (extracted-ont
-	  (make-v3kb :name module-uri
+	  (make-v3kb :name module-iri
 		     :manager manager
 		     :ont extracted
 		     :datafactory (#"getOWLDataFactory" manager)
 		     :weakened-from ontology)))
-    (let ((entities (loop for class in (append (and include-object-properties (set-to-list (#"getObjectPropertiesInSignature" extracted)))
-					       (and include-data-properties (set-to-list (#"getDataPropertiesInSignature" extracted)))
-					       (and include-annotation-properties (set-to-list (#"getAnnotationPropertiesInSignature" extracted)))
-					       (set-to-list (#"getClassesInSignature" extracted)))
-		       with table = (make-hash-table :test 'equal)
-		       do (setf (gethash (#"toString" (#"getIRI" class)) table ) t)
-		       finally (return table))))
-      (each-axiom ontology 
-		  (lambda(ax)
-		    (axiom-typecase ax
-		      (:AnnotationAssertion
-		       (when (gethash (#"toString" (#"getSubject" ax)) entities)
-			 (#"add" changes (#"addAxiom" manager extracted ax))))
-		      (:Declaration
-		       (if (eq (owl-declaration-type ax) :annotation-property)
-			   (#"add" changes (#"addAxiom" manager extracted ax))))
-		      ))
-		  t)
-;      (inspect changes)
-;      (#"applyChanges"  manager changes)
+
+    ;; collect all entities in the extracted ontology so we can copy annotations for them
+    (let ((entities (loop for el in (append  (set-to-list (#"getObjectPropertiesInSignature" extracted))
+					     (set-to-list (#"getDataPropertiesInSignature" extracted))
+					     (set-to-list (#"getAnnotationPropertiesInSignature" extracted))
+					     (set-to-list (#"getClassesInSignature" extracted)))
+		          with table = (make-hash-table :test 'equal)
+		          do (setf (gethash (#"toString" (#"getIRI" el)) table ) t)
+		          finally (return table))))
+      ;; Copy over annotations from the source ontology
+      (let ((sig-as-strings (and label-imported
+                                 (loop with table = (make-hash-table :test 'equalp)
+                                  for el in (j2list signature)
+                                  do (setf (gethash (#"toString" (#"getIRI" el)) table) t)
+                                  finally (return table)))))
+        (each-axiom ontology 
+	    (lambda(ax)
+	      (axiom-typecase ax
+		(:AnnotationAssertion
+		 (when (gethash (#"toString" (#"getSubject" ax)) entities)
+                   (if (and (equalp "rdfs:label"
+                                    (#"toString" (#"getProperty" ax)))
+                            label-imported
+                            (not (gethash (#"toString" (#"getSubject" ax)) sig-as-strings)))
+                       (progn 
+                         (#"add" changes (#"addAxiom"
+                                          manager extracted
+                                          (to-owlapi-axiom
+                                           `(annotation-assertion ,!rdfs:label
+                                                                  ,(make-uri (#"toString" (#"getSubject" ax)))
+                                                                  ,(append-ontology-parenthetical (#"toString"
+                                                                                                   (or (ignore-errors (#"getLiteral" (#"getValue" ax)))
+                                                                                                       (#"getValue" ax)))
+                                                                                                  (#"toString" (#"getSubject" ax)))
+                                                                  ) extracted-ont)))
+                         (#"add" changes (#"removeAxiom" manager extracted ax)))
+		       (#"add" changes (#"addAxiom" manager extracted ax)))))
+		(:Declaration
+		 (if (eq (owl-declaration-type ax) :annotation-property)
+		     (#"add" changes (#"addAxiom" manager extracted ax))))
+		))
+	  t))
+      ;; If dest provided, write the module there
       (when dest
-	(write-rdfxml extracted dest))
-      extracted)))
+	(write-rdfxml extracted-ont dest))
+      ;; Return the module ontology object
+      extracted-ont)))
+
+;; get an ontology label from an IRI
+;; First check OBO iris, XXX_dddddd and use XXX when found
+;; Otherwise use the second to last component of the IRI, where the last component is either after # or /
+
+(defun append-ontology-parenthetical  (value subject)
+  (let ((ont (or (caar (all-matches subject "(.*/([^_]+)_.*)" 2))
+                 (caar (all-matches subject "(.*/(.*)[/#][^/]*)" 2)))))
+    (if (equal ont "CommonCoreOntologies") (setq ont "cco"))
+    (#"replaceFirst" value "[\"]?([^@\"]+)[\"]?(@[a-zA-Z-]+)?" (format nil "$1 (~a)$2" (string-upcase ont)))))
 
 (defun test-make-ndpo-module ()
   ;(save-ontology-and-imports-locally "http://ccdb.ucsd.edu/NDPO/1.0/NDPO.owl" "/Users/alanr/Desktop/save/")
